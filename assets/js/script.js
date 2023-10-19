b0469e00f036e2b37693444bf4ceef30c628b412
Kurt Riecken <kurt.riecken@gmail.com>
2023-10-19 07:19:40 -0400

    strings playing correct pitches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 const eStringSound = new Audio("./assets/audio/E_string_pluck.mp3");
+const aStringSound = new Audio("./assets/audio/A_string_pluck.mp3");
+const dStringSound = new Audio("./assets/audio/D_string_pluck.mp3");
+const gStringSound = new Audio("./assets/audio/G_string_pluck.mp3");
+
 
 const strings = document.querySelectorAll('.string');
 const startButton = document.getElementById('start_button');
@@ -17,7 +21,21 @@ function showBass() {
 function pluckString(event) {
     const string = event.target;
     string.style.animation = 'vibrate 0.2s ease-in-out';
-    eStringSound.play();
+    console.log(event.target.id);
+    switch (event.target.id) {
+        case "string1":
+            eStringSound.play();
+            break;
+        case "string2":
+            aStringSound.play();
+            break;
+        case "string3":
+            dStringSound.play();
+            break;
+        case "string4":
+            gStringSound.play();
+            break;
+    }
     setTimeout(() => {
         string.style.animation = '';
     }, 200);
